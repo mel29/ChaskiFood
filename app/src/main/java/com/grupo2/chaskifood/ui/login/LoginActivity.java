4756f468fce058012c1a94e425cc4e7de5040f9d
Jose Alzugaray <jose.alzugaray@lundero.com>
2023-11-10 22:04:49 -0500

    Initial commit

---
+++ app/src/main/java/com/grupo2/chaskifood/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.grupo2.chaskifood.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.grupo2.chaskifood.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
